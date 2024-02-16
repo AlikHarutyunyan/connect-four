a6ec6ac7afba6275cc04d44039e0e381eb6f32b9
Alik <alikharutyunyan00@gmail.com>
2024-02-16 14:09:15 +0200

    Capital P

--- src/OpponentChoose.js
+++ src/OpponentChoose.js
@@ -5,7 +5,7 @@ import connectBot from "./images/connect4bot.jpg";
 import twoPlayer2 from "./images/2player_v2.jpg";
 
 const opponents = [
-    { id: Constants.TWO_PLAYER, image: twoPlayer2, name: "2 player", description: "Grab your friend and play a game of connect4 together" },
+    { id: Constants.TWO_PLAYER, image: twoPlayer2, name: "2 Player", description: "Grab your friend and play a game of connect4 together" },
     { id: Constants.MR_RANDOM, image: mrRandom, name: "Mr. Random", description: "Mr.Random doesn't even think about his move, he will immediately make his move randomly after yours" },
     { id: Constants.CONNECT_FOUR_BOT, image: connectBot, name: "Connect4Bot", description: "Connect4Bot is a robot that was created to always win in 7x7 board, good luck beating him!" }
 ];
